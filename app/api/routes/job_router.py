dbdc337fb4afdc02c3f9f9c907a2a730021c7a88
Bhavyai Gupta <zbhavyai@gmail.com>
2025-12-20 22:04:24 -0700

    fix handling of a couple of exceptions

--- app/api/routes/job_router.py
+++ app/api/routes/job_router.py
@@ -1,7 +1,8 @@
+import asyncio
 import logging
 from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import FileResponse
 
 from app.schemas.job_schema import Job, JobAcknowledgment, JobRequest
@@ -19,7 +20,12 @@ job_manager = JobManager()
 async def request_generation(request: JobRequest) -> JobAcknowledgment:
     logger.debug("request_generation")
 
-    return await job_manager.submit_job(request)
+    try:
+        jobAck = await job_manager.submit_job(request)
+        return JobAcknowledgment.model_validate(jobAck)
+    except asyncio.QueueFull:
+        logger.error("Job queue is full")
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Queue is full")
 
 
 @router.get(
@@ -30,11 +36,16 @@ async def get_job_status(job_id: UUID) -> Job:
     logger.debug("get_job_status")
 
     job = job_manager.get_job(job_id)
+
+    if job is None:
+        logger.error("Job not found with id=%s", job_id)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Job not found")
+
     return Job.model_validate(job)
 
 
-@router.get(
-    "/download/{filename}",
+@router.post(
+    "{job_id}",
     response_class=FileResponse,
 )
 async def download_file(filename: str) -> FileResponse:
